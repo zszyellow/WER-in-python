3289ab2836bd46786c92ce8a31ef1f4b7d066361
zszyellow <2581113549@qq.com>
2017-11-07 15:17:45 -0500

    make the structure of this file clearer

--- wer.py
+++ wer.py
@@ -4,18 +4,23 @@
 import sys
 import numpy
 
-def wer(r, h):
-    """
-    This is a function that calculate the word error rate in ASR.
-    You can use it like this: wer("what is it".split(), "what is".split()) 
-    """
-    #build the matrix
+def editDistance(r, h):
+    '''
+    This funciton is to calculate the edit distance of refernce sentence and the hypothesis sentence.
+    Main algorithm used is dynamic programming.
+
+    Attributes: 
+        r -> the list of words produced by splitting reference sentence.
+        h -> the list of words produced by splitting hypothesis sentence.
+    '''
     d = numpy.zeros((len(r)+1)*(len(h)+1), dtype=numpy.uint8).reshape((len(r)+1, len(h)+1))
     for i in range(len(r)+1):
         for j in range(len(h)+1):
-            if i == 0: d[0][j] = j
-            elif j == 0: d[i][0] = i
-    for i in range(1,len(r)+1):
+            if i == 0: 
+                d[0][j] = j
+            elif j == 0: 
+                d[i][0] = i
+    for i in range(1, len(r)+1):
         for j in range(1, len(h)+1):
             if r[i-1] == h[j-1]:
                 d[i][j] = d[i-1][j-1]
@@ -24,64 +29,80 @@ def wer(r, h):
                 insert = d[i][j-1] + 1
                 delete = d[i-1][j] + 1
                 d[i][j] = min(substitute, insert, delete)
-    result = float(d[len(r)][len(h)]) / len(r) * 100
-    result = str("%.2f" % result) + "%"
+    return d
 
-    #find out the manipulation steps
+def getStepList(r, h, d):
+    '''
+    This function is to get the list of steps in the process of dynamic programming.
+
+    Attributes: 
+        r -> the list of words produced by splitting reference sentence.
+        h -> the list of words produced by splitting hypothesis sentence.
+        d -> the matrix built when calulating the editting distance of h and r.
+    '''
     x = len(r)
     y = len(h)
     list = []
     while True:
         if x == 0 and y == 0: 
             break
+        elif d[x][y] == d[x-1][y-1] and r[x-1] == h[y-1] and x >= 1 and y >= 1: 
+            list.append("e")
+            x = x - 1
+            y = y - 1
+        elif d[x][y] == d[x][y-1]+1 and y >= 1:
+            list.append("i")
+            x = x
+            y = y - 1
+        elif d[x][y] == d[x-1][y-1]+1 and x >= 1 and y >= 1:
+            list.append("s")
+            x = x - 1
+            y = y - 1
         else:
-            if d[x][y] == d[x-1][y-1] and r[x-1] == h[y-1] and x-1>=0 and y-1>=0: 
-                list.append("e")
-                x = x-1
-                y = y-1
-            elif d[x][y] == d[x][y-1]+1 and y-1>=0:
-                list.append("i")
-                x = x
-                y = y-1
-            elif d[x][y] == d[x-1][y-1]+1 and x-1>=0 and y-1>=0:
-                list.append("s")
-                x = x-1
-                y = y-1
-            else:
-                list.append("d")
-                x = x-1
-                y = y
-    list = list[::-1]
-    #print the result in aligned way
+            list.append("d")
+            x = x - 1
+            y = y
+    return list[::-1]
+
+def alignedPrint(list, r, h, result):
+    '''
+    This funcition is to print the result of comparing reference and hypothesis sentences in an aligned way.
+    
+    Attributes:
+        list   -> the list of steps.
+        r      -> the list of words produced by splitting reference sentence.
+        h      -> the list of words produced by splitting hypothesis sentence.
+        result -> the rate calculated based on edit distance.
+    '''
     print "REF:",
     for i in range(len(list)):
         if list[i] == "i":
             count = 0
             for j in range(i):
                 if list[j] == "d":
-                    count += 1;
+                    count += 1
             index = i - count
             print " "*(len(h[index])),
         elif list[i] == "s":
             count1 = 0
             for j in range(i):
                 if list[j] == "i":
-                    count1 += 1;
+                    count1 += 1
             index1 = i - count1
             count2 = 0
             for j in range(i):
                 if list[j] == "d":
-                    count2 += 1;
+                    count2 += 1
             index2 = i - count2
-            if len(r[index1])<len(h[index2]):
-                print r[index1]+" "*(len(h[index2])-len(r[index1])),
+            if len(r[index1]) < len(h[index2]):
+                print r[index1] + " " * (len(h[index2])-len(r[index1])),
             else:
                 print r[index1],
         else:
             count = 0
             for j in range(i):
                 if list[j] == "i":
-                    count += 1;
+                    count += 1
             index = i - count
             print r[index],
     print
@@ -91,29 +112,29 @@ def wer(r, h):
             count = 0
             for j in range(i):
                 if list[j] == "i":
-                    count += 1;
+                    count += 1
             index = i - count
-            print " "*(len(r[index])),
+            print " " * (len(r[index])),
         elif list[i] == "s":
             count1 = 0
             for j in range(i):
                 if list[j] == "i":
-                    count1 += 1;
+                    count1 += 1
             index1 = i - count1
             count2 = 0
             for j in range(i):
                 if list[j] == "d":
-                    count2 += 1;
+                    count2 += 1
             index2 = i - count2
-            if len(r[index1])>len(h[index2]):
-                print h[index2]+" "*(len(r[index1])-len(h[index2])),
+            if len(r[index1]) > len(h[index2]):
+                print h[index2] + " " * (len(r[index1])-len(h[index2])),
             else:
                 print h[index2],
         else:
             count = 0
             for j in range(i):
                 if list[j] == "d":
-                    count += 1;
+                    count += 1
             index = i - count
             print h[index],
     print
@@ -123,40 +144,56 @@ def wer(r, h):
             count = 0
             for j in range(i):
                 if list[j] == "i":
-                    count += 1;
+                    count += 1
             index = i - count
-            print "D"+" "*(len(r[index])-1),
+            print "D" + " " * (len(r[index])-1),
         elif list[i] == "i":
             count = 0
             for j in range(i):
                 if list[j] == "d":
-                    count += 1;
+                    count += 1
             index = i - count
-            print "I"+" "*(len(h[index])-1),
+            print "I" + " " * (len(h[index])-1),
         elif list[i] == "s":
             count1 = 0
             for j in range(i):
                 if list[j] == "i":
-                    count1 += 1;
+                    count1 += 1
             index1 = i - count1
             count2 = 0
             for j in range(i):
                 if list[j] == "d":
-                    count2 += 1;
+                    count2 += 1
             index2 = i - count2
-            if len(r[index1])>len(h[index2]):
-                print "S"+" "*(len(r[index1])-1),
+            if len(r[index1]) > len(h[index2]):
+                print "S" + " " * (len(r[index1])-1),
             else:
-                print "S"+" "*(len(h[index2])-1),
+                print "S" + " " * (len(h[index2])-1),
         else:
             count = 0
             for j in range(i):
                 if list[j] == "i":
-                    count += 1;
+                    count += 1
             index = i - count
-            print " "*(len(r[index])),
+            print " " * (len(r[index])),
     print
-    print "WER: "+result    
+    print "WER: " + result
+
+def wer(r, h):
+    """
+    This is a function that calculate the word error rate in ASR.
+    You can use it like this: wer("what is it".split(), "what is".split()) 
+    """
+    # build the matrix
+    d = editDistance(r, h)
+
+    # find out the manipulation steps
+    list = getStepList(r, h, d)
+
+    # print the result in aligned way
+    result = float(d[len(r)][len(h)]) / len(r) * 100
+    result = str("%.2f" % result) + "%"
+    alignedPrint(list, r, h, result)
 
 if __name__ == '__main__':
     filename1 = sys.argv[1]
